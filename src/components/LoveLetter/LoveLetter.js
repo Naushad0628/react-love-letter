58f5e6f2203c31ce9eafdb9201347f6d3f62e9cc
MD NAUSHAD ALI <91078093+Naushad0628@users.noreply.github.com>
2024-06-20 20:51:12 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,10 +35,10 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma🖤,<br />
-        never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
-        always yours,<br />
-        @niihaaarrrr
+        Happy 13th Birthday to my wonderful little pp brother!🖤,<br />
+        Welcome to your teenage years! Watching you grow into the amazing person you are has been such a joy. Our family is so proud of you and all that you've accomplished. As you step into this exciting new chapter, know that we will always be here for you, no matter what.<br />i loveeee youuuu :)<br />
+        Your curiosity, kindness, and determination inspire us every day. We are so lucky to have you in our family. Here’s to a year filled with joy, growth, and unforgettable moments. Enjoy your special day to the fullest!,<br />
+        @MOHAMMAD NAUSHAD ALI
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
