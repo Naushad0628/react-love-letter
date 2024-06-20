fe030da3de197f9dbb410283ad583e1283311468
MD NAUSHAD ALI <91078093+Naushad0628@users.noreply.github.com>
2024-06-20 20:52:01 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,7 +35,7 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        Happy 13th Birthday to my wonderful little pp brother!🖤,<br />
+        Happy Birthday to my wonderful little pp brother!🖤,<br />
         Welcome to your teenage years! Watching you grow into the amazing person you are has been such a joy. Our family is so proud of you and all that you've accomplished. As you step into this exciting new chapter, know that we will always be here for you, no matter what.<br />i loveeee youuuu :)<br />
         Your curiosity, kindness, and determination inspire us every day. We are so lucky to have you in our family. Here’s to a year filled with joy, growth, and unforgettable moments. Enjoy your special day to the fullest!,<br />
         @MOHAMMAD NAUSHAD ALI
